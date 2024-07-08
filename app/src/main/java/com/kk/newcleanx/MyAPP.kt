08819c9a794fcf966cd8c0a4f6de2a7e7700b10e
KK <919578485@qq.com>
2024-07-08 11:11:50 +0800

    初始化

--- app/src/main/java/com/kk/newcleanx/MyAPP.kt
+++ app/src/main/java/com/kk/newcleanx/MyAPP.kt
@@ -2,12 +2,14 @@ package com.kk.newcleanx
 
 import android.app.Application
 import com.kk.newcleanx.data.local.app
+import com.tencent.mmkv.MMKV
 
 class MyAPP:Application() {
 
     override fun onCreate() {
         super.onCreate()
         app = this
+        MMKV.initialize(this)
     }
 
 }
