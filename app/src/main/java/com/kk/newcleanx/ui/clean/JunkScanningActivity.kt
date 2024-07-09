2a21661a945f41c23dc4f47809822b138a3f0001
KK <919578485@qq.com>
2024-07-09 19:48:19 +0800

    扫描动画及弹窗

--- app/src/main/java/com/kk/newcleanx/ui/clean/JunkScanningActivity.kt
+++ app/src/main/java/com/kk/newcleanx/ui/clean/JunkScanningActivity.kt
@@ -5,10 +5,13 @@ import android.content.Intent
 import android.graphics.PorterDuff
 import android.os.Bundle
 import android.view.View
+import androidx.activity.addCallback
 import androidx.core.content.ContextCompat
 import com.kk.newcleanx.R
 import com.kk.newcleanx.databinding.AcJunkScanningBinding
 import com.kk.newcleanx.ui.base.AllFilePermissionActivity
+import com.kk.newcleanx.ui.common.dialog.CustomAlertDialog
+import com.kk.newcleanx.utils.startRotateAnim
 
 class JunkScanningActivity : AllFilePermissionActivity<AcJunkScanningBinding>() {
 
@@ -30,7 +33,37 @@ class JunkScanningActivity : AllFilePermissionActivity<AcJunkScanningBinding>() 
             toolbar.ivBack.setOnClickListener {
                 onBackPressedDispatcher.onBackPressed()
             }
+            onBackPressedDispatcher.addCallback { onBackClicked() }
+            requestAllFilePermission {
+                if (it) {
+                    setStartAnim()
+                } else finish()
+            }
+
         }
+    }
+
+
+    private fun setStartAnim() {
+        binding.apply {
+            ivCacheLoading.startRotateAnim()
+            ivApkLoading.startRotateAnim()
+            ivLogLoading.startRotateAnim()
+            ivAdLoading.startRotateAnim()
+            ivTempLoading.startRotateAnim()
+        }
+    }
+
+    private fun onBackClicked() {
+        CustomAlertDialog(this).showDialog(title = getString(R.string.string_tips),
+                                           message = getString(R.string.string_scanning_stop_tip),
+                                           positiveButtonText = getString(R.string.string_ok),
+                                           negativeButtonText = getString(R.string.string_cancel),
+                                           onPositiveButtonClick = {
+                                               it.dismiss()
+                                               finish()
+                                           },
+                                           onNegativeButtonClick = {})
     }
 
 
--- app/src/main/java/com/kk/newcleanx/utils/CommonExt.kt
+++ app/src/main/java/com/kk/newcleanx/utils/CommonExt.kt
@@ -3,7 +3,11 @@ package com.kk.newcleanx.utils
 import android.content.Context
 import android.content.res.Resources
 import android.graphics.Point
+import android.view.View
 import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.LinearInterpolator
+import android.view.animation.RotateAnimation
 import kotlin.math.log10
 import kotlin.math.pow
 
@@ -26,4 +30,15 @@ fun Long.formatStorageSize(): String = let {
     val digitGroups = (log10(this.toDouble()) / log10(1000.0)).toInt()
     return String.format("%.1f %s", this / 1000.0.pow(digitGroups.toDouble()), units[digitGroups])
 
+}
+
+fun View.startRotateAnim() = run {
+    runCatching {
+        this.startAnimation(RotateAnimation(0f, 360f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f).apply {
+            interpolator = LinearInterpolator()
+            repeatCount = Animation.INFINITE
+            duration = 800L
+            fillAfter = true
+        })
+    }
 }
