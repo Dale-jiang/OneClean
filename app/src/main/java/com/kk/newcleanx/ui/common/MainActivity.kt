1bcd3b2662f1c0130fc1882e0d929a4b516091ec
KK <919578485@qq.com>
2024-07-10 21:29:01 +0800

    空文件夹扫描动画

--- app/src/main/java/com/kk/newcleanx/ui/common/MainActivity.kt
+++ app/src/main/java/com/kk/newcleanx/ui/common/MainActivity.kt
@@ -15,8 +15,9 @@ import com.kk.newcleanx.data.local.DEVICE_STATUS
 import com.kk.newcleanx.data.local.EMPTY_FOLDER
 import com.kk.newcleanx.databinding.AcMainBinding
 import com.kk.newcleanx.ui.base.AllFilePermissionActivity
-import com.kk.newcleanx.ui.functions.clean.JunkScanningActivity
 import com.kk.newcleanx.ui.common.adapter.MainListAdapter
+import com.kk.newcleanx.ui.functions.clean.JunkScanningActivity
+import com.kk.newcleanx.ui.functions.empty.EmptyFolderActivity
 import com.kk.newcleanx.utils.CommonUtils
 import com.kk.newcleanx.utils.formatStorageSize
 import kotlinx.coroutines.Dispatchers
@@ -56,7 +57,11 @@ class MainActivity : AllFilePermissionActivity<AcMainBinding>() {
                 BIG_FILE_CLEAN -> {}
                 APP_MANAGER -> {}
                 DEVICE_STATUS -> {}
-                EMPTY_FOLDER -> {}
+                EMPTY_FOLDER -> {
+                    requestAllFilePermission { success ->
+                        if (success) EmptyFolderActivity.start(this)
+                    }
+                }
             }
         }
 
--- app/src/main/java/com/kk/newcleanx/ui/functions/empty/EmptyFolderActivity.kt
+++ app/src/main/java/com/kk/newcleanx/ui/functions/empty/EmptyFolderActivity.kt
@@ -5,11 +5,12 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.activity.viewModels
-import com.kk.newcleanx.databinding.AcJunkScanningBinding
+import androidx.core.view.isVisible
+import com.kk.newcleanx.databinding.AcEmptyFolderBinding
 import com.kk.newcleanx.ui.base.AllFilePermissionActivity
 import com.kk.newcleanx.ui.functions.clean.vm.JunkCleanViewModel
 
-class EmptyFolderActivity : AllFilePermissionActivity<AcJunkScanningBinding>() {
+class EmptyFolderActivity : AllFilePermissionActivity<AcEmptyFolderBinding>() {
     companion object {
         fun start(context: Context) {
             context.startActivity(Intent(context, EmptyFolderActivity::class.java))
@@ -24,6 +25,17 @@ class EmptyFolderActivity : AllFilePermissionActivity<AcJunkScanningBinding>() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding.apply {
+            startProgress(minWaitTime = 3000L) {
+                if (it >= 100) clLoading.isVisible = false
+            }
+            isCompleted = true
+            toolbar.ivBack.setOnClickListener {
+                if (clLoading.isVisible) return@setOnClickListener
+                finish()
+            }
+        }
 
     }
 }
