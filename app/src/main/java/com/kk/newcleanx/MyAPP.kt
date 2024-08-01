9c0ae53928595a84e75f1f2f32fae5b2f0487179
KK <919578485@qq.com>
2024-08-01 17:14:56 +0800

    gradle配置

--- app/src/main/java/com/kk/newcleanx/MyAPP.kt
+++ app/src/main/java/com/kk/newcleanx/MyAPP.kt
@@ -2,6 +2,8 @@ package com.kk.newcleanx
 
 import android.app.Application
 import com.google.android.gms.ads.MobileAds
+import com.google.firebase.Firebase
+import com.google.firebase.initialize
 import com.kk.newcleanx.data.local.app
 import com.kk.newcleanx.ui.functions.admob.ADManager
 import com.kk.newcleanx.utils.AppLifecycleHelper
@@ -14,6 +16,7 @@ class MyAPP:Application() {
         app = this
         MMKV.initialize(this)
         registerActivityLifecycleCallbacks(AppLifecycleHelper())
+        Firebase.initialize(this)
         MobileAds.initialize(this)
         ADManager.initData()
     }
