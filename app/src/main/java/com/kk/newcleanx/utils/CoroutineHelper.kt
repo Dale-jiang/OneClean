dc918b5cc3877f05158365635053d8aac342546f
KK <919578485@qq.com>
2024-08-02 17:03:26 +0800

    提测

--- app/src/main/java/com/kk/newcleanx/utils/CoroutineHelper.kt
+++ app/src/main/java/com/kk/newcleanx/utils/CoroutineHelper.kt
@@ -1,25 +1,18 @@
 package com.kk.newcleanx.utils
 
-import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.asCoroutineDispatcher
 import kotlinx.coroutines.launch
-import java.util.concurrent.Executors
 
 object CoroutineHelper {
 
-    private val availableProcessors = Runtime.getRuntime().availableProcessors()
-    private val poolSize = availableProcessors * 2
-    private val customIODispatcher: CoroutineDispatcher = Executors.newFixedThreadPool(poolSize).asCoroutineDispatcher()
-
-    private val iOScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
-    private val mainScope = CoroutineScope(SupervisorJob() + Dispatchers.Main)
+    private val iOScope by lazy { CoroutineScope(SupervisorJob() + Dispatchers.IO) }
+    private val mainScope by lazy { CoroutineScope(SupervisorJob() + Dispatchers.Main) }
 
     fun launchIO(block: suspend () -> Unit): Job {
-        return iOScope.launch(customIODispatcher) {
+        return iOScope.launch {
             try {
                 block()
             } catch (e: Exception) {
