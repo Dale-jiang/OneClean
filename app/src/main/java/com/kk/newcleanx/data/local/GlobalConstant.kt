00b8d195b04a2caf727765037efd65a8db6ba9e2
KK <919578485@qq.com>
2024-08-06 17:00:41 +0800

    参数修改

--- app/src/main/java/com/kk/newcleanx/data/local/GlobalConstant.kt
+++ app/src/main/java/com/kk/newcleanx/data/local/GlobalConstant.kt
@@ -4,8 +4,8 @@ package com.kk.newcleanx.data.local
 const val INTENT_KEY = "intent_key"
 
 const val BLANK_URL = "about:blank"
-const val USER_AGREEMENT_URL = "https://www.baidu.com"
-const val USER_PRIVACY_URL = "https://www.baidu.com"
+const val USER_AGREEMENT_URL = "https://sites.google.com/view/oneclean-termsofservice"
+const val USER_PRIVACY_URL = "https://sites.google.com/view/oneclean-privacy-policy"
 
 const val BIG_FILE_CLEAN = "big_file_clean"
 const val APP_MANAGER = "app_manager"
