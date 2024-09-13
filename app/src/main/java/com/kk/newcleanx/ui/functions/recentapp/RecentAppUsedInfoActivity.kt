238d7183caaaf895bf7aee3467f7e2ec80df5a67
KK <919578485@qq.com>
2024-09-13 18:17:33 +0800

    算法优化

--- app/src/main/java/com/kk/newcleanx/ui/functions/recentapp/RecentAppUsedInfoActivity.kt
+++ app/src/main/java/com/kk/newcleanx/ui/functions/recentapp/RecentAppUsedInfoActivity.kt
@@ -6,17 +6,25 @@ import android.graphics.PorterDuff
 import android.os.Bundle
 import android.view.View
 import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
 import com.kk.newcleanx.R
 import com.kk.newcleanx.databinding.AcRecentAppUsedInfoBinding
-import com.kk.newcleanx.ui.base.BaseActivity
+import com.kk.newcleanx.ui.base.AllFilePermissionActivity
+import com.kk.newcleanx.ui.functions.admob.ADManager
 import com.kk.newcleanx.ui.functions.recentapp.fragment.AppLaunchesFragment
 import com.kk.newcleanx.ui.functions.recentapp.fragment.ScreenTimeInfoFragment
+import com.kk.newcleanx.utils.tba.TbaHelper
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
-class RecentAppUsedInfoActivity : BaseActivity<AcRecentAppUsedInfoBinding>() {
+class RecentAppUsedInfoActivity : AllFilePermissionActivity<AcRecentAppUsedInfoBinding>() {
     companion object {
         fun start(context: Context) {
             context.startActivity(Intent(context, RecentAppUsedInfoActivity::class.java))
@@ -32,9 +40,23 @@ class RecentAppUsedInfoActivity : BaseActivity<AcRecentAppUsedInfoBinding>() {
 
         binding.apply {
 
+            startProgress(minWaitTime = 2000L) {
+                if (it >= 100) {
+                    showFullAd {
+                        clLoading.isVisible = false
+                        viewLottie.cancelAnimation()
+                    }
+                }
+            }
+
             toolbar.tvTitle.text = getString(R.string.recent_app)
             toolbar.ivBack.setColorFilter(ContextCompat.getColor(this@RecentAppUsedInfoActivity, R.color.main_text_color), PorterDuff.Mode.SRC_IN)
             toolbar.ivBack.setOnClickListener {
+                if (clLoading.isVisible) return@setOnClickListener
+                onBackPressedDispatcher.onBackPressed()
+            }
+
+            ivScanBack.setOnClickListener {
                 onBackPressedDispatcher.onBackPressed()
             }
 
@@ -60,6 +82,11 @@ class RecentAppUsedInfoActivity : BaseActivity<AcRecentAppUsedInfoBinding>() {
             viewPager.isUserInputEnabled = true
             viewPager.adapter = ViewPageAdapter(this@RecentAppUsedInfoActivity, fragmentList)
             viewPager.offscreenPageLimit = 2
+
+            lifecycleScope.launch(Dispatchers.Main) {
+                delay(3000L)
+                isCompleted = true
+            }
         }
     }
 
@@ -81,6 +108,28 @@ class RecentAppUsedInfoActivity : BaseActivity<AcRecentAppUsedInfoBinding>() {
                 screenTime.setTextColor(ContextCompat.getColor(this@RecentAppUsedInfoActivity, R.color.white))
             }
 
+        }
+    }
+
+    private fun showFullAd(b: () -> Unit) {
+
+        if (ADManager.isOverAdMax() || ADManager.isBlocked()) {
+            b.invoke()
+            return
+        }
+
+        // log : oc_scan_int
+        TbaHelper.eventPost("oc_ad_chance", hashMapOf("ad_pos_id" to "oc_scan_int"))
+        lifecycleScope.launch {
+            while (!lifecycle.currentState.isAtLeast(Lifecycle.State.RESUMED)) delay(200L)
+            if (ADManager.ocScanIntLoader.canShow(this@RecentAppUsedInfoActivity)) {
+                ADManager.ocScanIntLoader.showFullScreenAd(this@RecentAppUsedInfoActivity, "oc_scan_int") {
+                    b.invoke()
+                }
+            } else {
+                ADManager.ocScanIntLoader.loadAd(this@RecentAppUsedInfoActivity)
+                b.invoke()
+            }
         }
     }
 
